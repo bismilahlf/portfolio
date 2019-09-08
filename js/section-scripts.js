5b8f870fca80f782641c59ff9498ee7317622238
satellitestudiodesign <juancarlos@satellitestud.io>
2019-09-08 13:57:54 +0200

    mobile styles

--- js/section-scripts.js
+++ js/section-scripts.js
@@ -1,5 +1,5 @@
-var sections = [...document.getElementsByClassName("content")];
-var asideSections = [...document.getElementsByClassName("asideSection")];
+var sections = [...document.getElementsByTagName("section")];
+var asideSections = [...document.getElementsByTagName("aside")];
 
 var scrollTopOffset = window.innerHeight * 0.7;
 var currentSection = -1;
@@ -15,10 +15,8 @@ function onScroll() {
   if (section !== currentSection) {
     for (let i = 0; i < sections.length; i++) {
       if (i === section) {
-        sections[i].style.opacity = 1;
         asideSections[i].style.opacity = 1;
       } else {
-        sections[i].style.opacity = 0.4;
         asideSections[i].style.opacity = 0;
       }
     }
@@ -37,10 +35,12 @@ try {
   window.removeEventListener("testPassive", null, opts);
 } catch (e) {}
 
-window.addEventListener(
-  "scroll",
-  onScroll,
-  supportsPassive ? { passive: true } : false
-);
+if (window.innerWidth > 1024) {
+  window.addEventListener(
+    "scroll",
+    onScroll,
+    supportsPassive ? { passive: true } : false
+  );
 
-onScroll();
+  onScroll();
+}
